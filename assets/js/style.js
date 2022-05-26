0b2beca984a2b2a55122e0877321566486a401c4
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-05-26 10:16:55 -0400

    add appID and apiKeys for recipe and nutrition APIs

---
+++ assets/js/style.js
@@ -0,0 +1,8 @@
+//for recipe api
+var recipeAppID = 650e07f6;
+var recipeApiKey = 61be0ff18cfbf722d2b2b7a832127896;
+
+// for nutrition api 
+var nutritionAppID = 9c414ff1;
+var nutritionApiKey = 8237150803039e132ee19b2cc6302444;
+
