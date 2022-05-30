b28e91085e9b5244fac421b8f11f8cdf01510934
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-05-29 22:47:07 -0400

    add random meal api, add html random button, add space in css for styling

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,6 +4,7 @@ var recipeApiKey = "61be0ff18cfbf722d2b2b7a832127896";
 
 var inputEl = document.getElementById("search-name");
 var searchButtonEl = document.getElementById("search-btn");
+var randomButtonEl = document.getElementById("random-btn");
 var containerEl = document.querySelector(".columns");
 
 searchButtonEl.addEventListener("click", function (event) {
@@ -42,9 +43,11 @@ function recipeHTML(results) {
         </div>
         <div class="card-content" id="recipe-content">
             <div class="media-content">
-                    <p class="title is-4 recipe-title" id="recipe-title"> 
+                    <p class="title is-4 recipe-title" id="recipe-title">
                     ${response.recipe.label}
                     </p>
+                    <h3 class = "source">
+                    Source: ${response.recipe.source}
             </div>
             <div class="nutrition-info" id="nutrition-info">
                 Servings: ${response.recipe.yield} <br>
@@ -56,10 +59,41 @@ function recipeHTML(results) {
             </footer>
         </div>`;
     containerEl.innerHTML = cards;
-<<<<<<< HEAD
   });
 }
-=======
-    })
+
+randomButtonEl.addEventListener("click", getRandomRecipe)
+
+async function getRandomRecipe() {
+  var randomUrl = "https://themealdb.com/api/json/v1/1/random.php"
+  console.log(randomUrl)
+  var response = await fetch(randomUrl);
+  var data = await response.json();
+  randomHTML(data.meals);
 }
->>>>>>> 29560a868a27aaee2bab91c2bb667275f440397b
+
+function randomHTML(results) {
+  console.log(results);
+  var cards = "";
+  results.map((response) => {
+    cards += `
+    <div class="card" id="recipe-card">
+        <div class="card-image recipe-image" id="recipe-image">
+            <figure class="image is-4by3">
+                <img src="${response.strMealThumb}" alt = "photo of recipe">
+            </figure>
+        </div>
+        <div class="card-content" id="recipe-content">
+            <div class="media-content">
+                    <p class="title is-4 recipe-title" id="recipe-title">
+                    ${response.strMeal} 
+                    </p>
+            </div>
+            <footer class="card-footer">
+                <a href=" ${response.strSource} " class="card-footer-item view-recipe" id="view-recipe" target= "_blank">View Recipe</a>
+                <a href="#" class="card-footer-item save-recipe" id="save-recipe">Save</a>
+            </footer>
+        </div>`;
+    containerEl.innerHTML = cards;
+  });
+}
